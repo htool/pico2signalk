782d54c411147d0f5f8997d853837a8273f351d8
Hans Tool <hans@defect.org>
2021-11-29 13:39:51 +0100

    Better way to deal with missing data.

--- pico.py
+++ pico.py
@@ -395,20 +395,13 @@ while True:
     sensorListTmp[sensorListTmp_id].update({'capacity.remaining': element[element_id][1] * stateOfCharge })
     sensorListTmp[sensorListTmp_id].update({'voltage': element[element_id + 2 ][1] / float(1000)})
     #sensorListTmp[sensorListTmp_id].update({'temperature': float(("%.2f" % round(element[48][1] / float(10) + 273.15, 2)))})
-    #debug( sensorListTmp )
 
     # Dynamo
     sensorListTmp_id = 31
     element_id = 49 
-    # stateOfCharge = float("%.2f" % (element[element_id][0] / 16000.0))
-    # debug("Dynamo%: " + str(stateOfCharge))
-    # sensorListTmp[sensorListTmp_id].update({'stateOfCharge': stateOfCharge })
-    # sensorListTmp[sensorListTmp_id].update({'capacity.remaining': element[element_id][1] * stateOfCharge })
-    voltage = element[element_id + 2 ][1] / float(1000)
-    if ( voltage == 65.54):  # Misread from data
-      voltage = 0
-    sensorListTmp[sensorListTmp_id].update({'voltage': voltage})
-    #sensorListTmp[sensorListTmp_id].update({'temperature': float(("%.2f" % round(element[48][1] / float(10) + 273.15, 2)))})
+    if (element[element_id +2][1] != 65535):
+      sensorListTmp[sensorListTmp_id].update({'voltage': element[element_id +2][1] / 1000})
+
     #debug( sensorListTmp )
 
     # Populate JSON
